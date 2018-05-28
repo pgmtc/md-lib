4f369c1197912bb7ac30ca0c8db567ac84a5b03d
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-05-28 13:17:31 +0100

    Adding api handler for local developments

--- src/MdPortletServer.js
+++ src/MdPortletServer.js
@@ -1,11 +1,14 @@
+import express from 'express'
 import log from './logger'
 import MdMessageHub from './MdMessageHub'
+import MdUtils from "./MdUtils"
 const MSGHUB_SERVER = process.env.MSGSERVER || undefined
 const MSGHUB_ID = process.env.MSGHUB_ID || 'mdesktop'
 
 export default class MdPortletServer {
   constructor(id) {
     log.debug(`Creating ${this.constructor.name} (id = ${id})`);
+
     if (typeof id === 'undefined' || id === null) {
       throw new Error('PortletServer needs id in a constructor');
     }
@@ -19,8 +22,27 @@ export default class MdPortletServer {
       log.error('Error when connecting to messaging server');
       process.exit(1)
     })
+  }
 
+  getRestApi() {
+    var router = express.Router();
+    router.get('/:methodName/:methodParams', ::this.handleApiCall)
+    router.get('/:methodName/', ::this.handleApiCall)
+    return router;
+  }
 
+  async handleApiCall(req, res, next){
+    var methodName = req.params.methodName;
+    var methodParams = MdUtils.getRestApiParams(req.params.methodParams);
+    log.debug(`Received api call for ${methodName}, parameters: ${methodParams}`)
+    // Invoke function
+    try {
+      var endpoint = MSGHUB_ID + '.' + this.id + '.' + methodName
+      var results = await this.invoke.apply(this, [endpoint].concat(methodParams))
+      res.send(results);
+    } catch (err) {
+      res.status(500).send(err.message)
+    }
   }
 
   destructor() {
