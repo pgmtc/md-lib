a536d9474fe5060e9b575ee6a39df41214a1e0c0
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-05-28 12:20:49 +0100

    adding express handler for api to utils

--- index.js
+++
@@ -1,3 +0,0 @@
-import Portlet from './src/Portlet'
-
-export default Portlet
--- src/MdUtils.js
+++ src/MdUtils.js
@@ -1,23 +1,49 @@
 import util from 'util'
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-var ARGUMENT_NAMES = /([^\s,]+)/g;
+const MSGHUB_ID = process.env.MSGHUB_ID || 'mdesktop'
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
+var ARGUMENT_NAMES = /([^\s,]+)/g
 
 export default class MdUtils {
-  static getFunctionParameters(func) {
+  static getFunctionParameters (func) {
     if (util.format(func).indexOf('Function: bound') > -1) {
-      return [];
+      return []
     }
-    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if (result === null)
-      result = [];
-    return result;
+    var fnStr = func.toString().replace(STRIP_COMMENTS, '')
+    var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES)
+    if (result === null) {
+      result = []
+    }
+    return result
   }
 
-  static getFunctionName(func) {
+  static getFunctionName (func) {
     var fDescr = util.format(func)
     var fName = fDescr.replace(/bound /, '').replace(/(Function:)|(bound)|[\[\] ]*/g, '')
-    return fName;
+    return fName
+  }
 
+  static getRestApiParams (encoded) {
+    let parsed = decodeURIComponent(encoded)
+    try {
+      parsed = JSON.parse(parsed)
+    } catch (err) {
+
+    }
+    return parsed
+  }
+
+  static async handleApiCall (req, res, next) {
+    var component = req.params.component
+    var method = req.params.method
+    var params = MdUtils.getRestApiParams(req.params.params)
+
+    // Invoke function
+    try {
+      var endpoint = MSGHUB_ID + '.' + component + '.' + method
+      var results = await this.invoke.apply(this, [endpoint].concat(params))
+      res.send(results)
+    } catch (err) {
+      res.status(500).send(err.message)
+    }
   }
 }
