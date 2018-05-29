d77739e9506109bb25d218d2c8102caa0b1a9b40
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-05-29 11:40:20 +0100

    library restructure

--- src/server/MdPortletServer.js
+++ src/server/MdPortletServer.js
@@ -2,23 +2,25 @@ import express from 'express'
 import path from 'path'
 import log from './logger'
 import MdMessageHub from './MdMessageHub'
-import MdUtils from "./MdUtils"
+import MdUtils from './MdUtils'
+
 const MSGHUB_SERVER = process.env.MSGSERVER || undefined
 const MSGHUB_ID = process.env.MSGHUB_ID || 'mdesktop'
+const app = express()
 
 export default class MdPortletServer {
-  constructor(id, portletLocation) {
-    log.debug(`Creating ${this.constructor.name} (id = ${id})`);
+  constructor (id, portletLocation) {
+    log.debug(`Creating ${this.constructor.name} (id = ${id})`)
     this.portletLocation = portletLocation
 
     // Cleanup on exit
-    process.on('SIGINT', ::this.destructor);
-    process.on('SIGUSR1', ::this.destructor);
-    process.on('SIGUSR2', ::this.destructor);
-    process.on('uncaughtException', ::this.destructor);
+    process.on('SIGINT', ::this.destructor)
+    process.on('SIGUSR1', ::this.destructor)
+    process.on('SIGUSR2', ::this.destructor)
+    process.on('uncaughtException', ::this.destructor)
 
     if (typeof id === 'undefined' || id === null) {
-      throw new Error('PortletServer needs id in a constructor');
+      throw new Error('PortletServer needs id in a constructor')
     }
 
     this.id = id
@@ -30,14 +32,16 @@ export default class MdPortletServer {
     this.msgHub.connect(MSGHUB_SERVER).then(() => {
       // Proxy invoke and expose methods
     }, (err) => {
-      log.error('Error when connecting to messaging server', err);
+      log.error('Error when connecting to messaging server', err)
       process.exit(1)
     })
   }
 
-  listen(port) {
-    this.app = express()
-    this.app.use('/', this.getRestApi())
+  listen (port) {
+    if (this.portletLocation) {
+      this.app.use('/', ::this.servePortlet)
+    }
+    this.app.use('/api', this.getRestApi())
     this.app.listen(port, (err) => {
       if (err) {
         log.error('Cannot start the server on port ' + port)
@@ -48,37 +52,34 @@ export default class MdPortletServer {
     })
   }
 
-  getRestApi() {
-    var router = express.Router();
+  getRestApi () {
+    var router = express.Router()
     router.get('/:methodName/:methodParams', ::this.handleApiCall)
     router.get('/:methodName/', ::this.handleApiCall)
-    if (this.portletLocation) {
-      router.get('/', ::this.servePortlet)
-    }
-    return router;
+    return router
   }
 
-  async servePortlet(req, res, next) {
+  async servePortlet (req, res, next) {
     res.sendFile(path.join(appDir, this.portletLocation))
   }
 
-  async handleApiCall(req, res, next) {
-    var methodName = req.params.methodName;
-    var methodParams = MdUtils.decodeApiParams(req.params.methodParams);
+  async handleApiCall (req, res, next) {
+    var methodName = req.params.methodName
+    var methodParams = MdUtils.decodeApiParams(req.params.methodParams)
     log.debug(`Received api call for ${methodName}, parameters: ${methodParams}`)
     // Invoke function
     try {
       var endpoint = MSGHUB_ID + '.' + this.id + '.' + methodName
       var results = await this.invoke.apply(this, [endpoint].concat(methodParams))
-      res.send(results);
+      res.send(results)
     } catch (err) {
       res.status(500).send(err.message)
     }
   }
 
-  destructor() {
+  destructor () {
     this.msgHub.disconnect()
-    process.exit(1);
+    process.exit(1)
   }
 
 }
