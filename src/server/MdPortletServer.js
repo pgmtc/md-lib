78b4245ddd5f1896c76b594ef3ef6b8f3bc317ab
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-06-25 23:49:53 +0100

    externalizing config as a constructor parameter

--- src/server/MdPortletServer.js
+++ src/server/MdPortletServer.js
@@ -1,20 +1,41 @@
-import express from 'express'
 import path from 'path'
 import log from './logger'
-import MdMessageHub from './MdMessageHub'
 import MdUtils from './MdUtils'
-import grpc from 'grpc'
-const protoLoader = require('@grpc/proto-loader')
 
-const MSGHUB_SERVER = process.env.MSGHUB_SERVER || undefined
 const MSGHUB_ID = process.env.MSGHUB_ID || 'mdesktop'
 const appDir = path.dirname(require.main.filename)
+var MdMessageHub = null
 
+/*
+portletLocation
+grpcDefLocation
+msgHubServer
+msgHubId
+enableNats
+enableHttp
+enableGrpc
+
+
+ */
 export default class MdPortletServer {
-  constructor (id, portletLocation, grpcDefLocation) {
-    log.debug(`Creating ${this.constructor.name} (id = ${id})`)
-    this.portletLocation = portletLocation
-    this.grpcDefLocation = grpcDefLocation
+  constructor (config) {
+    this.config = config || {
+      enableNats: false,
+      enableHttp: true,
+      enableGrpc: false
+    }
+
+    if (this.config.enableNats) {
+      MdMessageHub = require('./MdMessageHub')
+    }
+
+    let validation = this.validate()
+    if (validation.length > 0) {
+      validation.forEach((message) => log.error(message))
+      process.exit(1)
+    }
+
+    log.debug(`Creating ${this.constructor.name} (id = ${this.config.id})`)
 
     // Cleanup on exit
     process.on('SIGINT', ::this.destructor)
@@ -22,51 +43,84 @@ export default class MdPortletServer {
     process.on('SIGUSR2', ::this.destructor)
     process.on('uncaughtException', ::this.destructor)
 
-    if (typeof id === 'undefined' || id === null) {
-      throw new Error('PortletServer needs id in a constructor')
+
+    let nats = this.config.enableNats
+    this.msgHub = nats ? new MdMessageHub(this.config.msgHubId, this.config.id) : null
+    this.invoke = nats ? this.msgHub::this.msgHub.invoke : this.noMessagingWarning
+    this.expose = nats ? this.msgHub::this.msgHub.expose : this.noMessagingWarning
+    this.exposeJob = nats? this.msgHub::this.msgHub.exposeJob : this.noMessagingWarning
+    this.publish = nats ? this.msgHub::this.msgHub.publish : this.noMessagingWarning
+    this.subscribe = nats ? this.msgHub::this.msgHub.subscribe : this.noMessagingWarning
+    if (nats) {
+      this.msgHub.connect(this.config.msgHubServer).then(() => {
+        log.info('Success when connecting to messaging server ' + this.config.msgHubServer)
+      }, (err) => {
+        log.error('Error when connecting to messaging server ' + this.config.msgHubServer, err)
+        process.exit(1)
+      })
     }
 
-    this.id = id
-    this.msgHub = new MdMessageHub(MSGHUB_ID, this.id)
-    this.invoke = this.msgHub::this.msgHub.invoke
-    this.expose = this.msgHub::this.msgHub.expose
-    this.exposeJob = this.msgHub::this.msgHub.exposeJob
-    this.publish = this.msgHub::this.msgHub.publish
-    this.subscribe = this.msgHub::this.msgHub.subscribe
-    this.msgHub.connect(MSGHUB_SERVER).then(() => {
-      log.info('Success when connecting to messaging server ' + MSGHUB_SERVER)
-    }, (err) => {
-      log.error('Error when connecting to messaging server ' + MSGHUB_SERVER, err)
-      process.exit(1)
-    })
+    if (this.config.enableHttp) {
+      let express = require('express')
+      this.app = express()
+      this.apiRouter = express.Router()
+    }
 
-    this.app = express()
-    this.apiRouter = express.Router()
-
-    this.grpcServer = new grpc.Server()
+    if (this.config.enableGrpc) {
+      let grpc = require('grpc')
+      this.grpcServer = new grpc.Server()
+    }
   }
 
+  validate() {
+    let cnf = this.config
+    if (!cnf) return ['Config is missing']
+    let errmsg = []
+    if (typeof cnf.id === 'undefined') errmsg.push('Config property id is missing')
+    if (cnf.enableNats &&!cnf.msgHubServer) errmsg.push('NATS enabled, but msgHubServer property is missing')
+    if (cnf.enableNats &&!cnf.msgHubId) errmsg.push('NATS enabled, but msgHubId property is missing')
+    return errmsg
+  }
+
+  noMessagingWarning() {
+    log.warn('Cannot perform requested operation as nats is disabled')
+  }
+
+  noHttpWarning() {
+    log.warn('Cannot perform requested operation as the http is not enabled')
+  }
+
+  noGrpcWarning() {
+    log.warn('Cannot perform requested operation as the http is not enabled')
+  }
+
+
   exposeGet (path, handler) {
+    if (!this.config.enableHttp) return this.noHttpWarning()
     log.info(`Exposing [GET] /api${path}`)
     this.apiRouter.get(path, handler)
   }
 
   exposePost (path, handler) {
+    if (!this.config.enableHttp) return this.noHttpWarning()
     log.info(`Exposing [POST] /api${path}`)
     this.apiRouter.post(path, handler)
   }
 
   exposePut (path, handler) {
+    if (!this.config.enableHttp) return this.noHttpWarning()
     log.info(`Exposing [PUT] /api${path}`)
     this.apiRouter.put(path, handler)
   }
 
   exposeDelete (path, handler) {
+    if (!this.config.enableHttp) return this.noHttpWarning()
     log.info(`Exposing [DELETE] /api${path}`)
     this.apiRouter.delete(path, handler)
   }
 
   exposeGrpc(method, methodNameOverride) {
+    if (!this.config.enableGrpc) return this.noGrpcWarning()
     if (!method || typeof(method) !== 'function') {
       log.error('exposeGrpc needs to be provided a function object')
       return
@@ -75,7 +129,7 @@ export default class MdPortletServer {
     this.exposedGrpc = this.exposedGrpc || {}
 
     let wrapper = (call, callback) => {
-      this.msgHub.msgHubLog(MdMessageHub.CODE.INVOKE)
+      if (this.config.enableNats) this.msgHub.msgHubLog(MdMessageHub.CODE.INVOKE)
       try {
         let results = method(call.request)
         callback(null, results)
@@ -89,14 +143,15 @@ export default class MdPortletServer {
   }
 
   listen (port) {
+    if (!this.config.enableHttp) return
     this.listenPort = port
     this.app.use('/api', this.apiRouter)
 
-    if (this.grpcDefLocation) {
+    if (this.config.grpcDefLocation) {
       this.app.use('/grpc', ::this.serveGrpcDef)
     }
 
-    if (this.portletLocation) {
+    if (this.config.portletLocation) {
       this.app.use('/', ::this.servePortlet)
     }
     this.expressServer = this.app.listen(port, (err) => {
@@ -110,6 +165,9 @@ export default class MdPortletServer {
   }
 
   listenGrpc (grpcPort) {
+    if (!this.config.enableGrpc) return
+    log.info(`Starting GRPC server on *:${grpcPort}`)
+    let grpc = require('grpc')
     this.grpcPort = grpcPort
     this._addGrpcService()
     this.grpcServer.bind('0.0.0.0:' + grpcPort, grpc.ServerCredentials.createInsecure())
@@ -118,8 +176,11 @@ export default class MdPortletServer {
   }
 
   _addGrpcService() {
+    if (!this.config.enableGrpc) return this.noGrpcWarning()
     try {
-      let grcpProto = protoLoader.loadSync(path.join(appDir, this.grpcDefLocation), {})
+      let grpc = require('grpc')
+      let protoLoader = require('@grpc/proto-loader')
+      let grcpProto = protoLoader.loadSync(path.join(appDir, this.config.grpcDefLocation), {})
       let grpcObjectDef = grpc.loadPackageDefinition(grcpProto)
       let defaultPkgName = Object.keys(grpcObjectDef)[0]
       let defaultPkgObject = grpcObjectDef[defaultPkgName]
@@ -133,11 +194,11 @@ export default class MdPortletServer {
 
 
   servePortlet (req, res, next) {
-    res.sendFile(path.join(appDir, this.portletLocation))
+    res.sendFile(path.join(appDir, this.config.portletLocation))
   }
 
   serveGrpcDef (req, res, next) {
-    res.sendFile(path.join(appDir, this.grpcDefLocation))
+    res.sendFile(path.join(appDir, this.config.grpcDefLocation))
   }
 
   async handleApiCall (req, res, next) {
@@ -146,7 +207,7 @@ export default class MdPortletServer {
     log.debug(`Received api call for ${methodName}, parameters: ${methodParams}`)
     // Invoke function
     try {
-      var endpoint = MSGHUB_ID + '.' + this.id + '.' + methodName
+      var endpoint = this.config.msgHubId + '.' + this.id + '.' + methodName
       var results = await this.invoke.apply(this, [endpoint].concat(methodParams))
       res.send(results)
     } catch (err) {
@@ -156,9 +217,9 @@ export default class MdPortletServer {
 
   destructor (err) {
     try {
-      this.msgHub.disconnect()
-      this.expressServer.close()
-      this.grpcServer.forceShutdown()
+      this.msgHub ? this.msgHub.disconnect() : null
+      this.expressServer ? this.expressServer.close() : null
+      this.grpcServer ? this.grpcServer.forceShutdown() : null
     } catch (err) {
       log.error(err)
     }
